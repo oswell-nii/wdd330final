d7bd55ae81ac71d9879864518af82077b518923c
oswell-oxy <oswellokine1999@gmail.com>
2025-08-09 19:56:54 +0000

    Project Structure

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        // eslint-disable-next-line no-undef
+        main: resolve(__dirname, "src/index.html")
+      },
+    },
+  },
+});
